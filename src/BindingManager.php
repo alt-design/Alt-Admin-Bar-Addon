d558a2bd22d91cb1be113193fe5ba086038829c3
benammi <benammi@alt-design.net>
2025-04-29 11:21:37 +0100

    Removed override for the DataRepository in BindingManager.php and removed the Data shim class since they're no longer required.

--- src/BindingManager.php
+++ src/BindingManager.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace AltDesign\AltAdminBar;
 
 use AltDesign\AltAdminBar\Helpers\Data;
-use AltDesign\AltAdminBar\Extend\Data as DataExtension;
-use Statamic\Contracts\Data\DataRepository;
 use Statamic\Filesystem\Manager;
 use Statamic\Stache\Stache;
 use Statamic\Yaml\Yaml;
@@ -27,21 +25,6 @@ class BindingManager
                 manager: resolve(Manager::class),
                 yaml: resolve(Yaml::class)
             );
-        });
-
-        $this->app->bind(DataExtension::class, function ($app) {
-            return new DataExtension();
-        });
-
-        $this->app->bind(DataRepository::class, function ($app) {
-            return (new DataExtension())
-                ->setRepository('entry', \Statamic\Contracts\Entries\EntryRepository::class)
-                ->setRepository('term', \Statamic\Contracts\Taxonomies\TermRepository::class)
-                ->setRepository('collection', \Statamic\Contracts\Entries\CollectionRepository::class)
-                ->setRepository('taxonomy', \Statamic\Contracts\Taxonomies\TaxonomyRepository::class)
-                ->setRepository('global', \Statamic\Contracts\Globals\GlobalRepository::class)
-                ->setRepository('asset', \Statamic\Contracts\Assets\AssetRepository::class)
-                ->setRepository('user', \Statamic\Contracts\Auth\UserRepository::class);
         });
 
         $this->app->singleton(
