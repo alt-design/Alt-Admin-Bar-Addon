65a3c2a384bda600a3efe19dbfaa6b41d7578c82
Alt-Charlie <charlie@alt-design.net>
2026-02-19 15:19:34 +0000

    Statamic V6 support

--- resources/views/bar.blade.php
+++ resources/views/bar.blade.php
@@ -1,22 +1,22 @@
 {!! $adminBarStyles !!}
 
-<div id="alt-admin-bar" class="alt-admin-bar alt-admin-bar-w-full alt-admin-bar-p-0 alt-admin-bar-flex alt-admin-bar-bg-[#333333] alt-admin-bar-text-sm alt-admin-bar-fixed alt-admin-bar-top-0 alt-admin-bar-z-[500] alt-admin-bar-justify-between">
-    <div class="alt-admin-bar-flex alt-admin-bar-text-white alt-admin-bar-items-center">
+<div id="alt-admin-bar" class="alt-admin-bar altadminbar:w-full altadminbar:p-0 altadminbar:flex altadminbar:bg-[#333333] altadminbar:text-sm altadminbar:fixed altadminbar:top-0 altadminbar:z-[500] altadminbar:justify-between">
+    <div class="altadminbar:flex altadminbar:text-white altadminbar:items-center">
 
         <!-- Non Negotiables -->
-        <a class="alt-admin-bar-px-4 alt-admin-bar-bg-gradient-to-r alt-admin-bar-from-pink-500 alt-admin-bar-via-yellow-400 alt-admin-bar-to-blue-500
-              alt-admin-bar-bg-400 alt-admin-bar-bg-clip-text alt-admin-bar-text-transparent alt-admin-bar-transition-all alt-admin-bar-duration-500
-              hover:alt-admin-bar-animate-gradient-x alt-admin-bar-font-bold" href="/">{{ config('app.name') }}</a>
+        <a class="altadminbar:px-4 altadminbar:bg-gradient-to-r altadminbar:from-pink-500 altadminbar:via-yellow-400 altadminbar:to-blue-500
+              altadminbar:bg-400 altadminbar:bg-clip-text altadminbar:text-transparent altadminbar:transition-all altadminbar:duration-500
+              altadminbar:hover:animate-gradient-x altadminbar:font-bold" href="/">{{ config('app.name') }}</a>
 
-        <a class="hover:alt-admin-bar-bg-[#555555] alt-admin-bar-px-4 alt-admin-bar-py-2 alt-admin-bar-flex" href="@antlers{{ cp_url }}@endantlers">
-            <svg class="alt-admin-bar-h-[18px] alt-admin-bar-mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><circle cx="7" cy="8.5" r="3.5" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></circle><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M7 5v3.5h3.5M9 22.5a6.979 6.979 0 0 0 1.5-4m4.5 4a6.979 6.979 0 0 1-1.5-4m-6.001 4h9M.5 15.5h23"></path><rect width="23" height="17" x=".5" y="1.5" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" rx="1" ry="1"></rect><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M13.5 7 15 5l3 2.5 2.5-3m-1 8V11m-2 1.5v-2m-2 2v-3m-2 3V11"></path></svg>
+        <a class="altadminbar:hover:bg-[#555555] altadminbar:px-4 altadminbar:py-2 altadminbar:flex" href="@antlers{{ cp_url }}@endantlers">
+            <svg class="altadminbar:h-[18px] altadminbar:mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><circle cx="7" cy="8.5" r="3.5" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></circle><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M7 5v3.5h3.5M9 22.5a6.979 6.979 0 0 0 1.5-4m4.5 4a6.979 6.979 0 0 1-1.5-4m-6.001 4h9M.5 15.5h23"></path><rect width="23" height="17" x=".5" y="1.5" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" rx="1" ry="1"></rect><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M13.5 7 15 5l3 2.5 2.5-3m-1 8V11m-2 1.5v-2m-2 2v-3m-2 3V11"></path></svg>
             <span>
                 Admin
             </span>
         </a>
 
-        <a class="hover:alt-admin-bar-bg-[#555555] alt-admin-bar-px-4 alt-admin-bar-py-2 alt-admin-bar-flex" href="@antlers{{ edit_url }}@endantlers">
-            <svg class="alt-admin-bar-h-[18px] alt-admin-bar-mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M19.479 8V2.5a2 2 0 0 0-2-2h-12a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h8l3 3v-3h1a2 2 0 0 0 1.721-.982"></path><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M1.485 3.764A2 2 0 0 0 .479 5.5v16a2 2 0 0 0 2 2h8a2 2 0 0 0 1.712-.967M5.479 3.5h4m-2 4.5V3.5M15.7 7.221l-4.2-1.2 1.2 4.2 7.179 7.179a2.121 2.121 0 0 0 3-3zm3.279 9.279 3-3M12.7 10.221l3-3M12.479 3.5h4m-10 8h4m-4 3h6.5m-6.5 3h9"></path></svg>
+        <a class="altadminbar:hover:bg-[#555555] altadminbar:px-4 altadminbar:py-2 altadminbar:flex" href="@antlers{{ edit_url }}@endantlers">
+            <svg class="altadminbar:h-[18px] altadminbar:mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M19.479 8V2.5a2 2 0 0 0-2-2h-12a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h8l3 3v-3h1a2 2 0 0 0 1.721-.982"></path><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M1.485 3.764A2 2 0 0 0 .479 5.5v16a2 2 0 0 0 2 2h8a2 2 0 0 0 1.712-.967M5.479 3.5h4m-2 4.5V3.5M15.7 7.221l-4.2-1.2 1.2 4.2 7.179 7.179a2.121 2.121 0 0 0 3-3zm3.279 9.279 3-3M12.7 10.221l3-3M12.479 3.5h4m-10 8h4m-4 3h6.5m-6.5 3h9"></path></svg>
             <span>
                 Edit
             </span>
@@ -24,21 +24,21 @@
         <!-- End -->
 
         @foreach($menuItems as $menuKey => $menuItem)
-            <div class="alt-admin-bar-group alt-admin-bar-relative">
-                <a class="hover:alt-admin-bar-bg-[#555555] alt-admin-bar-px-4 alt-admin-bar-py-2 alt-admin-bar-inline-block" href="{{ $menuItem->href ?? '#' }}">
+            <div class="altadminbar:group altadminbar:relative">
+                <a class="altadminbar:hover:bg-[#555555] altadminbar:px-4 altadminbar:py-2 altadminbar:inline-block" href="{{ $menuItem->href ?? '#' }}">
                     {{ $menuItem->title }}
                 </a>
 
                 @if( $menuItem->hasChildren )
-<div class="alt-admin-bar-opacity-0 alt-admin-bar-pointer-events-none group-hover:alt-admin-bar-opacity-100 group-hover:alt-admin-bar-pointer-events-auto alt-admin-bar-absolute alt-admin-bar-top-full alt-admin-bar-left-0 alt-admin-bar-max-w-[400px] alt-admin-bar-bg-[#333333] alt-admin-bar-z-[500] alt-admin-bar-transition-opacity alt-admin-bar-duration-300">
+                    <div class="altadminbar:opacity-0 altadminbar:pointer-events-none  altadminbar:group-hover:opacity-100  altadminbar:group-hover:pointer-events-auto altadminbar:absolute altadminbar:top-full altadminbar:left-0 altadminbar:max-w-[400px] altadminbar:bg-[#333333] altadminbar:z-[500] altadminbar:transition-opacity altadminbar:duration-300">
                         @foreach($menuItem->children as $childKey => $child )
                             @if ($child->post)
                                 <form method="POST" action="{{ $child->href }}">
                                     @csrf
-                                    <button class="alt-admin-bar-px-3 alt-admin-bar-w-full alt-admin-bar-text-left alt-admin-bar-py-1.5 hover:alt-admin-bar-bg-[#555555] alt-admin-bar-whitespace-nowrap">{{ $child->title }}</button>
+                                    <button class="altadminbar:px-3 altadminbar:w-full altadminbar:text-left altadminbar:py-1.5 altadminbar:hover:bg-[#555555] altadminbar:whitespace-nowrap">{{ $child->title }}</button>
                                 </form>
                             @else
-                                <a class="alt-admin-bar-px-3 alt-admin-bar-w-full alt-admin-bar-text-left alt-admin-bar-py-1.5 hover:alt-admin-bar-bg-[#555555] alt-admin-bar-mb-2 alt-admin-bar-inline-block alt-admin-bar-whitespace-nowrap" href="{{ $child->href }}" >{{ $child->title }}</a>
+                                <a class="altadminbar:px-3 altadminbar:w-full altadminbar:text-left altadminbar:py-1.5 altadminbar:hover:bg-[#555555] altadminbar:mb-2 altadminbar:inline-block altadminbar:whitespace-nowrap" href="{{ $child->href }}" >{{ $child->title }}</a>
                             @endif
                         @endforeach
                     </div>
@@ -47,74 +47,74 @@
         @endforeach
     </div>
 
-    <div class="alt-admin-bar-flex alt-admin-bar-text-white alt-admin-bar-items-center">
+    <div class="altadminbar:flex altadminbar:text-white altadminbar:items-center">
         @if($revisionsEnabled)
-        <div class="alt-admin-bar-group alt-admin-bar-relative">
-            <a class="alt-admin-bar-px-4 alt-admin-bar-py-2 alt-admin-bar-flex alt-admin-bar-items-center hover:alt-admin-bar-bg-[#555555]
-            @if($activeEpoch)alt-admin-bar-px-4 alt-admin-bar-bg-gradient-to-r alt-admin-bar-from-pink-500 alt-admin-bar-via-yellow-400 alt-admin-bar-to-blue-500
-              alt-admin-bar-bg-400 alt-admin-bar-bg-clip-text alt-admin-bar-text-transparent alt-admin-bar-transition-all alt-admin-bar-duration-500
-              hover:alt-admin-bar-animate-gradient-x @endif" href="{{ $profileUrl }}">
+        <div class="altadminbar:group altadminbar:relative">
+            <a class="altadminbar:px-4 altadminbar:py-2 altadminbar:flex altadminbar:items-center altadminbar:hover:bg-[#555555]
+            @if($activeEpoch)altadminbar:px-4 altadminbar:bg-gradient-to-r altadminbar:from-pink-500 altadminbar:via-yellow-400 altadminbar:to-blue-500
+              altadminbar:bg-400 altadminbar:bg-clip-text altadminbar:text-transparent altadminbar:transition-all altadminbar:duration-500
+              altadminbar:hover:animate-gradient-x @endif" href="{{ $profileUrl }}">
                 Revisions
             </a>
             <div class="
-                alt-admin-bar-opacity-0
-                alt-admin-bar-px-2
-                group-hover:alt-admin-bar-opacity-100
-                alt-admin-bar-absolute alt-admin-bar-top-full
-                alt-admin-bar-right-0
-                alt-admin-bar-max-w-[400px]
-                alt-admin-bar-min-w-56
-                alt-admin-bar-py-1.5
-                alt-admin-bar-bg-[#333333]
-                alt-admin-bar-overflow-scroll
-                alt-admin-bar-max-h-64">
+                altadminbar:opacity-0
+                altadminbar:px-2
+                 altadminbar:group-hover:opacity-100
+                altadminbar:absolute altadminbar:top-full
+                altadminbar:right-0
+                altadminbar:max-w-[400px]
+                altadminbar:min-w-56
+                altadminbar:py-1.5
+                altadminbar:bg-[#333333]
+                altadminbar:overflow-scroll
+                altadminbar:max-h-64">
                 <a href="{{
-                        route('alt-admin-bar.revision.set',
+                        cp_route('alt-admin-bar.revision.set',
                         array_merge(
                             $revisionHrefData,
                             [ 'epoch' => null ]
                         )) }}">
                     <div @class([
-                            'alt-admin-bar-px-3',
-                            'alt-admin-bar-py-1.5',
-                            'alt-admin-bar-mb-2',
-                            'alt-admin-bar-w-full',
-                            'alt-admin-bar-min-w-36',
-                            'alt-admin-bar-inline-block',
-                            'alt-admin-bar-whitespace-nowrap',
-                            'alt-admin-bar-text-left',
-                            'hover:alt-admin-bar-bg-[#555555]',
-                            'alt-admin-bar-bg-white' => ! $activeEpoch,
-                            'alt-admin-bar-text-[#333333]' => ! $activeEpoch,
-                            'hover:alt-admin-bar-text-white' => ! $activeEpoch,
+                            'altadminbar:px-3',
+                            'altadminbar:py-1.5',
+                            'altadminbar:mb-2',
+                            'altadminbar:w-full',
+                            'altadminbar:min-w-36',
+                            'altadminbar:inline-block',
+                            'altadminbar:whitespace-nowrap',
+                            'altadminbar:text-left',
+                            'altadminbar:hover:bg-[#555555]',
+                            'altadminbar:bg-white' => ! $activeEpoch,
+                            'altadminbar:text-[#333333]' => ! $activeEpoch,
+                            'altadminbar:hover:text-white' => ! $activeEpoch,
                             ])>
                         <div>Published Version</div>
-                        <div class="alt-admin-bar-text-xs">{{ now()->format('y/m/d H:i') }}</div>
+                        <div class="altadminbar:text-xs">{{ now()->format('y/m/d H:i') }}</div>
                     </div>
                 </a>
                 @forelse($revisions as $epoch => $revision)
                     <a href="{{
-                        route('alt-admin-bar.revision.set',
+                        cp_route('alt-admin-bar.revision.set',
                         array_merge(
                             $revisionHrefData,
                             [ 'epoch' => $epoch ]
                         )) }}">
                         <div @class([
-                            'alt-admin-bar-px-3',
-                            'alt-admin-bar-py-1.5',
-                            'alt-admin-bar-mb-2',
-                            'alt-admin-bar-w-full',
-                            'alt-admin-bar-min-w-36',
-                            'alt-admin-bar-inline-block',
-                            'alt-admin-bar-whitespace-nowrap',
-                            'alt-admin-bar-text-left',
-                            'hover:alt-admin-bar-bg-[#555555]',
-                            'alt-admin-bar-bg-white' => $epoch == $activeEpoch,
-                            'alt-admin-bar-text-[#333333]' => $epoch == $activeEpoch,
-                            'hover:alt-admin-bar-text-white' => $epoch == $activeEpoch,
+                            'altadminbar:px-3',
+                            'altadminbar:py-1.5',
+                            'altadminbar:mb-2',
+                            'altadminbar:w-full',
+                            'altadminbar:min-w-36',
+                            'altadminbar:inline-block',
+                            'altadminbar:whitespace-nowrap',
+                            'altadminbar:text-left',
+                            'altadminbar:hover:bg-[#555555]',
+                            'altadminbar:bg-white' => $epoch == $activeEpoch,
+                            'altadminbar:text-[#333333]' => $epoch == $activeEpoch,
+                            'altadminbar:hover:text-white' => $epoch == $activeEpoch,
                             ])>
                             <div>{{ $revision->message() }}</div>
-                            <div class="alt-admin-bar-text-xs">{{ \Carbon\Carbon::parse($epoch)->format('y/m/d H:i') }}</div>
+                            <div class="altadminbar:text-xs">{{ \Carbon\Carbon::parse($epoch)->format('y/m/d H:i') }}</div>
                         </div>
                     </a>
                 @empty
@@ -123,17 +123,17 @@
             </div>
         </div>
         @endif
-        <div class="alt-admin-bar-group alt-admin-bar-relative">
-            <a class="alt-admin-bar-px-4 alt-admin-bar-py-2 alt-admin-bar-flex alt-admin-bar-items-center hover:alt-admin-bar-bg-[#555555]" href="{{ $profileUrl }}">
+        <div class="altadminbar:group altadminbar:relative">
+            <a class="altadminbar:px-4 altadminbar:py-2 altadminbar:flex altadminbar:items-center altadminbar:hover:bg-[#555555]" href="{{ $profileUrl }}">
                 <span>Hey there {{ auth()->user()->name ?? '' }}!</span>
-                <div class="alt-admin-bar-bg-[#555555] alt-admin-bar-uppercase alt-admin-bar-ml-3 alt-admin-bar-w-[22px] alt-admin-bar-h-[22px] alt-admin-bar-rounded-full alt-admin-bar-flex alt-admin-bar-items-center alt-admin-bar-justify-center">
+                <div class="altadminbar:bg-[#555555] altadminbar:uppercase altadminbar:ml-3 altadminbar:w-[22px] altadminbar:h-[22px] altadminbar:rounded-full altadminbar:flex altadminbar:items-center altadminbar:justify-center">
                     {{ $avatar }}
                 </div>
             </a>
-<div class="alt-admin-bar-opacity-0 alt-admin-bar-pointer-events-none group-hover:alt-admin-bar-opacity-100 group-hover:alt-admin-bar-pointer-events-auto alt-admin-bar-absolute alt-admin-bar-top-full alt-admin-bar-right-0 alt-admin-bar-max-w-[400px] alt-admin-bar-py-1.5 alt-admin-bar-bg-[#333333] alt-admin-bar-transition-opacity alt-admin-bar-duration-300">
-                <a class="alt-admin-bar-pl-3 alt-admin-bar-pr-8 alt-admin-bar-w-full alt-admin-bar-text-left alt-admin-bar-py-1.5 hover:alt-admin-bar-bg-[#555555] alt-admin-bar-mb-2 alt-admin-bar-inline-block alt-admin-bar-whitespace-nowrap" href="{{ $preferencesUrl }}">Preferences</a>
-                <a class="alt-admin-bar-pl-3 alt-admin-bar-pr-8 alt-admin-bar-w-full alt-admin-bar-text-left alt-admin-bar-py-1.5 hover:alt-admin-bar-bg-[#555555] alt-admin-bar-mb-2 alt-admin-bar-inline-block alt-admin-bar-whitespace-nowrap" href="{{ $profileUrl }}">Profile</a>
-                <a class="alt-admin-bar-pl-3 alt-admin-bar-pr-8 alt-admin-bar-w-full alt-admin-bar-text-left alt-admin-bar-py-1.5 hover:alt-admin-bar-bg-[#555555] alt-admin-bar-mb-2 alt-admin-bar-inline-block alt-admin-bar-whitespace-nowrap" href="{{ cp_route('logout') }}">Logout</a>
+            <div class="altadminbar:opacity-0 altadminbar:pointer-events-none  altadminbar:group-hover:opacity-100  altadminbar:group-hover:pointer-events-auto altadminbar:absolute altadminbar:top-full altadminbar:right-0 altadminbar:max-w-[400px] altadminbar:py-1.5 altadminbar:bg-[#333333] altadminbar:transition-opacity altadminbar:duration-300">
+                <a class="altadminbar:pl-3 altadminbar:pr-8 altadminbar:w-full altadminbar:text-left altadminbar:py-1.5 altadminbar:hover:bg-[#555555] altadminbar:mb-2 altadminbar:inline-block altadminbar:whitespace-nowrap" href="{{ $preferencesUrl }}">Preferences</a>
+                <a class="altadminbar:pl-3 altadminbar:pr-8 altadminbar:w-full altadminbar:text-left altadminbar:py-1.5 altadminbar:hover:bg-[#555555] altadminbar:mb-2 altadminbar:inline-block altadminbar:whitespace-nowrap" href="{{ $profileUrl }}">Profile</a>
+                <a class="altadminbar:pl-3 altadminbar:pr-8 altadminbar:w-full altadminbar:text-left altadminbar:py-1.5 altadminbar:hover:bg-[#555555] altadminbar:mb-2 altadminbar:inline-block altadminbar:whitespace-nowrap" href="{{ cp_route('logout') }}">Logout</a>
             </div>
         </div>
     </div>
